5e471f2fc32f6f75231d2f4bdd543ab5daf049f8
TizKah <tizianomanuel@gmail.com>
2024-02-25 00:29:23 -0300

    Create matriz functions

--- array/matriz/matriz_bi.c
+++ array/matriz/matriz_bi.c
@@ -5,34 +5,45 @@
 */
 
 struct Matriz_ {
-
-  /*Defina la estructura aqui*/
-
+  float** direccion;
+  size_t numFilas;
+  size_t numColumnas;
 };
 
 
 Matriz* matriz_crear(size_t numFilas, size_t numColumnas) {
+  Matriz* matriz = malloc(sizeof(Matriz));
 
+  matriz->numFilas = numFilas;
+  matriz->numColumnas = numColumnas;
+  matriz->direccion = malloc(sizeof(float) * numFilas);
+
+  for(int pos = 0; pos < numFilas; pos++)
+    matriz->direccion[pos] = malloc(sizeof(float) * numColumnas);
+
+  return matriz;
 }
 
 void matriz_destruir(Matriz* matriz) {
-
+  for(int pos = 0; pos < matriz->numFilas; pos++)
+    free(matriz->direccion[pos]);
+  free(matriz->direccion);
 }
 
 double matriz_leer(Matriz* matriz, size_t fil, size_t col) {
-
+  return matriz->direccion[fil][col];
 }
 
 void matriz_escribir(Matriz* matriz, size_t fil, size_t col, double val) {
-
+  matriz->direccion[fil][col] = val;
 }
 
 size_t matriz_num_filas(Matriz* matriz) {
-
+  return matriz->numFilas;
 }
 
 size_t matriz_num_columnas(Matriz* matriz) {
-
+  return matriz->numColumnas;
 }
 
 
--- array/matriz/matriz_uni.c
+++ array/matriz/matriz_uni.c
@@ -5,34 +5,39 @@
 */
 
 struct Matriz_ {
-
-  /*Defina la estructura aqui*/
-
+  size_t numFilas;
+  size_t numColumnas;
+  float* direccion;
 };
 
 
 Matriz* matriz_crear(size_t numFilas, size_t numColumnas) {
-
+  Matriz* matriz = malloc(sizeof(Matriz));
+  matriz->numFilas = numFilas;
+  matriz->numColumnas = numColumnas;
+  matriz->direccion = calloc(numFilas * numColumnas, sizeof(float));
+  return matriz;
 }
 
 void matriz_destruir(Matriz* matriz) {
-
+  free(matriz->direccion);
+  free(matriz);
 }
 
 double matriz_leer(Matriz* matriz, size_t fil, size_t col) {
-
+  return matriz->direccion[matriz->numColumnas * fil + col];
 }
 
 void matriz_escribir(Matriz* matriz, size_t fil, size_t col, double val) {
-
+  matriz->direccion[fil * matriz->numColumnas, col] = val;
 }
 
 size_t matriz_num_filas(Matriz* matriz) {
-
+  return matriz->numFilas;
 }
 
 size_t matriz_num_columnas(Matriz* matriz) {
-
+  return matriz->numColumnas;
 }
 
 
